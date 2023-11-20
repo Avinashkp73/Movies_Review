7ceefa8f87f617c967243b6f6f4edfe06ba2838c
Avinash Kumar <theavinashkp73@gmail.com>
2023-11-20 18:17:32 +0530

    header changed successfully

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import NavbarItem from "./NavbarItem";
 export default function Navbar() {
   return (
     <div className="flex justify-center dark:bg-gray-600 bg-cyan-100 lg:text-lg p-4">
-      <NavbarItem title="Trending Movies /Series" param="fetchTrending" />
-      <NavbarItem title="Top Rated Movies /Series" param="fetchTopRated" />
+      <NavbarItem title="Trending Movies" param="fetchTrending" />
+      <NavbarItem title="Top Rated Movies" param="fetchTopRated" />
     </div>
   );
 }
